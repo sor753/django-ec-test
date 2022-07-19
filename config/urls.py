75a3d5747958b1cb2266dcdfda255c4a042a4fd0
show kawazoe <show753@icloud.com>
2022-07-19 18:37:27 +0900

    add custom user

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 """config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    # Account
     path('admin/', admin.site.urls),
+    path('login/', views.Login.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('signup/', views.SignUpView.as_view()),
+    path('account/', views.AccountUpdateView.as_view()),
+    path('profile/', views.ProfileUpdateView.as_view()),
+
+    # Pay
+    path('pay/checkout/', views.PayWithStripe.as_view()),
+    path('pay/success/', views.PaySuccessView.as_view()),
+    path('pay/cancel/', views.PayCancelView.as_view()),
+
+    # Cart
+    path('cart/add/', views.AddCartView.as_view()),
+    path('cart/remove/<str:pk>/', views.remove_from_cart),
+    path('cart/', views.CartListView.as_view()),
+
+    # Items
+    path('items/<str:pk>/', views.ItemDetailView.as_view()),
+
+    path('', views.IndexListView.as_view()),
 ]
